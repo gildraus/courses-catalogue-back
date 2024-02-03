2589fc263b0ea072aa26d6f543cd35ef8ddd92b0
gildrausNV <nikola.vujcic.001@gmail.com>
2024-02-03 15:17:20 +0100

    adding vercel.json

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const jwt = require("jsonwebtoken");
 const authToken = require("./authentication/authenticateToken");
 const app = express();
 
-const corsOptions = {
-  origin: ["https://courses-catalogue-front.vercel.app", "http://localhost:3000"],
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
+// const corsOptions = {
+//   origin: ["https://courses-catalogue-front.vercel.app", "http://localhost:3000"],
+//   methods: "GET,POST,PUT,DELETE",
+//   allowedHeaders: "Content-Type,Authorization",
+// };
 
 
 //Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //Root route
 app.get("/", (req, res) => {
