d62fe80b17408e46683011e7a98a8bddaf24b2be
gildrausNV <nikola.vujcic.001@gmail.com>
2024-02-03 15:08:54 +0100

    all origins fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const authToken = require("./authentication/authenticateToken");
 const app = express();
 
 const corsOptions = {
-  origin: ["https://courses-catalogue-front.vercel.app", "http://localhost:3000"],
+  origin: "*",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
